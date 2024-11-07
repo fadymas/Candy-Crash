e5d236c4f1a07b008e78caa1196232c35219505b
fady <fadymahros373@gmail.com>
2024-11-08 00:05:52 +0200

    Make the grid and Put the Nodes

--- candyCrash.cpp
+++ candyCrash.cpp
@@ -1,3 +1,18 @@
 #include "candyCrash.h"
 #include <iostream>
+#include <vector>
 using namespace std;
+
+Node::Node(int pX, int pY, const string &candyColor, bool hasStar) : positionX(pX), positionY(pY), candyColor(candyColor), hasStar(hasStar), left(nullptr), right(nullptr), up(nullptr), down(nullptr) {};
+
+Grid::Grid(int const size, vector<vector<Node *>> nodes) : size(size), nodes(nodes)
+{
+    nodes.resize(size, vector<Node *>(size, nullptr));
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < size; j++)
+        {
+            nodes[i][j] = new Node(i, j, "");
+        }
+    }
+}
--- candyCrash.h
+++ candyCrash.h
@@ -1,4 +1,32 @@
-#ifndef CandyCrash
-#define CandyCrash
+#ifndef CANDYCRASH_H
+#define CANDYCRASH_H
+#include <string>
+#include <vector>
+using namespace std;
+
+struct Node
+{
+public:
+    Node *left;
+    Node *right;
+    Node *up;
+    Node *down;
+    string candyColor;
+    int positionX;
+    int positionY;
+    bool hasStar;
+
+    Node(int pX, int pY, const string &candyColor, bool hasStar = false);
+};
+
+class Grid
+{
+private:
+    int const size;
+    vector<vector<Node *>> nodes;
+
+public:
+    Grid(int const size, vector<vector<Node *>> nodes);
+};
 
 #endif
